f78d032784a76eaf3d0e037a7a0a5564dce1ad65
Maciej Malinowski <maciej.malinowski7@gmail.com>
2020-06-07 21:44:47 +0200

    Stats Project

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,18 +1,20 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
 
-const matches = fs
-  .readFileSync('football.csv', { encoding: 'utf-8' })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+//Create obj that satisfies DataReader interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create instance of MatchReader and pass in smt satisfying DataReader interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+for (let match of matchReader.matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
